24627fd4767f091387e33d779f46bef60d30e0c7
Harsh verma <harshverma.agc@gmail.com>
2023-07-19 01:49:05 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Column Widget"),
+        ),
+        body: Container(
+          color: Colors.blueAccent,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            )
+          ],
+        ),
+        )
+      ),
+    );
+  }
+}
